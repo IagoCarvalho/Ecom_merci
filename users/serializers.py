415bd3434fc0ac93b052d6da4217136b1405cf80
João Henrique Egewarth <egewarth@gmail.com>
2017-10-30 09:55:08 -0200

    Criando serializer de users

---
+++ users/serializers.py
@@ -0,0 +1,25 @@
+from .models import (CustomerUser, PhoneNumber, CreditCard, ShippingAddress)
+from rest_framework import serializers
+
+class PhoneNumberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PhoneNumber
+        fields = '__all__'
+
+
+class CustomerUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerUser
+        fields = '__all__'
+
+
+class CreditCardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CreditCard
+        fields = '__all__'
+
+
+class ShippingAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShippingAddress
+        fields = '__all__'
