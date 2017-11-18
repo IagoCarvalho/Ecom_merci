db344056aa0c89b4e585afae26fa51a1bf2651d9
IagoCarvalho <IagoVC_2012@hotmail.com>
2017-11-18 20:05:09 -0200

    Implementing gulp script to watch template files

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+// Importando plugins
+var gulp = require('gulp');
+var plugin = require('gulp-plugin');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var concat = require('gulp-concat');
+var autoReload = require('gulp-auto-reload');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+const del = require('del');
+var gutil = require('gulp-util');
+var nano = require('gulp-cssnano');
+
+// Default
+gulp.task('default', ['watch']);
+
+// Caminhos para fazer a build
+var paths = {
+  html: "Ecom_merci/**/*.html",
+  css: "Ecom_merci/**/*.css"
+};
+
+// logs
+gulp.task('css-log', function(){
+	console.log('CSS modified!');
+});
+
+gulp.task('js-log', function(){
+	console.log('JS modified!');
+});
+
+gulp.task('html-log', function(){
+	console.log('HTML modified!');
+});
+
+// Vigiar arquivos
+gulp.task('watch', function() {
+	gulp.watch('Ecom_merci/**/*.css', ['css-log']);
+	gulp.watch('Ecom_merci/**/*.js', ['js-log']);
+	gulp.watch('Ecom_merci/*.html', ['html-log']);
+});
+
+gulp.task('build', ['watch'], function() {
+});
